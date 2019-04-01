051f6244a6f63b3bf74c53b992a7145471d2852a
daailuo <136199108@qq.com>
2019-04-01 21:09:28 +0800

    t
    提交文件

---
+++ yychatServer/src/com/yychat/model/Message.java
@@ -0,0 +1,28 @@
+package com.yychat.model;
+
+public class Message {
+   private String sender;
+   private String receiver;
+   //private String content;
+   private String messageTapy;
+public String getSender() {
+	return sender;
+}
+public void setSender(String sender) {
+	this.sender = sender;
+}
+public String getReceiver() {
+	return receiver;
+}
+public void setReceiver(String receiver) {
+	this.receiver = receiver;
+}
+public String getMessageTapy() {
+	return messageTapy;
+}
+public void setMessageTapy(String messageTapy) {
+	this.messageTapy = messageTapy;
+} 
+   
+   
+}
